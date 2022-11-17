89897a54fcd33ad884ff07e2bf08dd3dd6da10c5
Your Name <you@example.com>
2022-11-17 16:02:19 +0300

    first commit

---
+++ app/src/main/java/com/example/solarsystem/finalActivity.kt
@@ -0,0 +1,13 @@
+package com.example.solarsystem
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class FinalActivity : AppCompatActivity() {
+    @Suppress("DEPRECATION")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_final)
+        window.decorView.apply { systemUiVisibility= View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_FULLSCREEN }}
+}
